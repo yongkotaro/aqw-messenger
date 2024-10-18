67122e23d1da29f48dff9d3cd2b133795c5700c1
yongkotaro <yongkotaro@gmail.com>
2024-10-18 15:32:23 +0800

    REST API to retrieve conversations

--- backend/controllers/conversation-controller.js
+++ backend/controllers/conversation-controller.js
@@ -1,5 +1,5 @@
 import Conversation from "../models/conversation-model.js";
-import User from "../models/user-model.js";
+import mongoose from "mongoose";
 
 export const createConversation = async (req, res) => {
     try {
@@ -7,6 +7,12 @@ export const createConversation = async (req, res) => {
 
         if (!name || !participants) {
             return res.status(400).json({ error: "Invalid data. Name and participants are required." });
+        }
+
+        for (const id of participants) {
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                return res.status(400).json({ error: `User does not exist` });
+            }
         }
 
         const newConversation = await Conversation.create({
@@ -20,6 +26,26 @@ export const createConversation = async (req, res) => {
         res.status(201).json(newConversation);
     } catch (error) {
         console.log("Error in createConversation controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getConversations = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        // Find conversations where the user is a participant
+        const conversations = await Conversation.find({
+            participants: userId,
+        });
+
+        // if (conversations.length === 0) {
+        //     return res.status(404).json({ error: "No conversations found for this user." });
+        // }
+
+        res.status(200).json(conversations);
+    } catch (error) {
+        console.log("Error in getUserConversations controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
--- backend/routes/conversation-routes.js
+++ backend/routes/conversation-routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createConversation } from "../controllers/conversation-controller.js";
+import { createConversation, getConversations } from "../controllers/conversation-controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, createConversation);
+router.get("/", protectRoute, getConversations);
 
 export default router;
