f071c1eedbb338a94f20cbd62e4dd90522823706
yongkotaro <yongkotaro@gmail.com>
2024-10-25 20:18:30 +0800

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth-routes.js';
 import messageRoutes from './routes/message-routes.js';
@@ -11,6 +12,8 @@ import connectToMongoDB from './db/connect-to-MongoDB.js';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -20,6 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes)
 app.use("/api/conversations", conversationRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 server.listen(PORT, () => {
     connectToMongoDB();
